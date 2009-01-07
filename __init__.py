6a95b45d4ffe67530569b3857d7b6dc54fbe6984
Simon Willison <simon@simonwillison.net>
2009-01-07 19:18:08 +0000

    Implementation of rate limiter

---
+++ __init__.py
@@ -0,0 +1,98 @@
+from django.http import HttpResponseForbidden
+from django.core.cache import cache
+from datetime import datetime, timedelta
+import functools
+
+class ratelimit(object):
+    "Instances of this class can be used as decorators"
+    # This class is designed to be sub-classed
+    minutes = 1 # The time period
+    requests = 10 # Number of allowed requests in that time period
+    
+    prefix = 'rl-' # Prefix for memcache key
+    expire_after = (minutes + 1) * 60
+    
+    def __init__(self, **options):
+        for key, value in options:
+            setattr(self, key, value)
+    
+    def __call__(self, fn):
+        def wrapper(request, *args, **kwargs):
+            return self.view_wrapper(request, fn, *args, **kwargs)
+        functools.update_wrapper(wrapper, fn)
+        return wrapper
+    
+    def view_wrapper(self, request, fn, *args, **kwargs):
+        if not self.should_ratelimit(request):
+            return fn(request, *args, **kwargs)
+        
+        counts = self.get_counters(request).values()
+        
+        # Increment rate limiting counter
+        self.cache_incr(self.current_key(request))
+        
+        # Have they failed?
+        if sum(counts) >= self.requests:
+            return self.disallowed(request)
+        
+        return fn(request, *args, **kwargs)
+    
+    def cache_get_many(self, keys):
+        return cache.get_many(keys)
+    
+    def cache_incr(self, key):
+        # memcache is only backend that can increment atomically
+        try:
+            cache._cache.incr(key, self.expire_after)
+        except AttributeError:
+            cache.set(key, cache.get(key, 0) + 1, self.expire_after)
+    
+    def should_ratelimit(self, request):
+        return True
+    
+    def get_counters(self, request):
+        return self.cache_get_many(self.keys_to_check(request))
+    
+    def keys_to_check(self, request):
+        extra = self.key_extra(request)
+        now = datetime.now()
+        return [
+            '%s%s-%s' % (
+                self.prefix,
+                extra,
+                (now - timedelta(minutes = minute)).strftime('%Y%m%d%H%M')
+            ) for minute in range(self.minutes + 1)
+        ]
+    
+    def current_key(self, request):
+        return '%s%s-%s' % (
+            self.prefix,
+            self.key_extra(request),
+            datetime.now().strftime('%Y%m%d%H%M')
+        )
+    
+    def key_extra(self, request):
+        # By default, their IP address is used
+        return request.META.get('REMOTE_ADDR', '')
+    
+    def disallowed(self, request):
+        "Over-ride this method if you want to log incidents"
+        return HttpResponseForbidden('Rate limit exceeded')
+
+
+def ratelimit_post(ratelimit):
+    "Rate limit POSTs - can be used to protect a login form"
+    key_field = None # If provided, this POST var will affect the rate limit
+    
+    def should_ratelimit(self, request):
+        return request.method == 'POST'
+    
+    def key_extra(self, request):
+        # IP address and key_field (if it is set)
+        extra = super(ratelimit_post).key_extra(request)
+        if self.key_field:
+            extra += '-' + request.POST.get(self.key_field, '')
+        return extra
+
+        
+    
--- readme.txt
+++ readme.txt
@@ -1,7 +1,49 @@
 ratelimitcache for Django
 =========================
+By Simon Willison - http://simonwillison.net/
 
 A rate limiter that uses Django's cache framework, with no requirement for a 
 persistent data store.
 
-By Simon Willison - http://simonwillison.net/
+Installation: Place the ratelimitcache.py on your Python path.
+
+Basic usage (max 20 requests every 3 minutes):
+
+    from ratelimitcache import ratelimit
+    
+    @ratelimit(minutes = 3, requests = 20)
+    def myview(request):
+        # ...
+        return HttpResponse('...')
+
+Protecting a login form, i.e rate limit on IP address and attempted username:
+    
+    from ratelimitcache import ratelimit_post
+    
+    @ratelimit_post(minutes = 3, requests = 10, key_field = 'username')
+    def login(request):
+        # ...
+        return HttpResponse('...')
+
+Custom behaviour, e.g. logging when the rate limit condition fails:
+    
+    from ratelimitcache import ratelimit
+    from my_logging_app.models import Log
+    import datetime, pprint
+    
+    class ratelimit_with_logging(ratelimit):
+        def disallowed(self, request):
+            Log.objects.create(
+                ip_address = request.META.get('REMOTE_ADDR'),
+                path = request.path,
+                counters = pprint.pformat(
+                    self.get_counters(reqest)
+                ),
+                created = datetime.datetime.now()
+            )
+            return HttpResponseForbidden('Rate limit exceeded')
+    
+    @ratelimit_with_logging(minutes = 3, requests = 20)
+    def myview(request):
+        # ...
+        return HttpResponse('...')
